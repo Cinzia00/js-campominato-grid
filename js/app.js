c947f24b1b3f6b3cf5b6289fd6d26b45634eef28
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-02-09 21:16:21 +0100

    Fix eventListener js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,42 @@
 console.log('ciao');
 
 let btnPlayElement = document.getElementById('btn-play');
-console.log(btnPlayElement)
+console.log(btnPlayElement);
 let latoGriglia = 10;
 let superficieGriglia = latoGriglia * latoGriglia;
-let LatoSecondaGriglia = 8;
-let superficieSecondaGriglia = LatoSecondaGriglia * LatoSecondaGriglia;
 console.log(latoGriglia, superficieGriglia);
+let grigliaAttiva = 0;
 
 btnPlayElement.addEventListener('click', function () {
-
-    // clickCounter++
-    for (let i = 0; i < superficieGriglia; i++) {
-        let num = i + 1
-        console.log(num)
-
-        // let grigliaDaStampare = createElement('div').style("width: calc(100% / &{latoGriglia}")
-        let grigliaDaStampare = `<div class="cella" style="width: calc(100% / ${latoGriglia});" >${num}</div>`
-        grigliaElement.innerHTML += grigliaDaStampare
-    }
-
-
-    if (clickCounter == 2) {
-        for (let i = 0; i < superficieSecondaGriglia; i++) {
+    if (grigliaElement.firstChild) {
+        grigliaElement.innerHTML = ''
+    } else {
+        for (let i = 0; i < superficieGriglia; i++) {
             let num = i + 1
-            console.log(num)
+            // console.log(num)
 
-            // let grigliaDaStampare = createElement('div').style("width: calc(100% / &{latoGriglia}")
-            let grigliaDaStampare = `<div class="cella" style="width: calc(100% / ${latoGriglia});" >${num}</div>`
-            grigliaElement.innerHTML += grigliaDaStampare
+            let grigliaDaStampare = document.createElement('div')
+            grigliaDaStampare.style.width = `calc(100% / ${latoGriglia})`
+            grigliaDaStampare.classList.add("cella")
+            // let grigliaDaStampare = `<div class="cella active" style="width: calc(100% / ${latoGriglia});" >${num}</div>`
+            // grigliaElement.innerHTML += grigliaDaStampare
+            grigliaDaStampare.innerHTML += num
+            grigliaElement.append(grigliaDaStampare)
         }
-
     }
 })
 
 
-
 const grigliaElement = document.querySelector('.griglia');
+
 console.log(grigliaElement);
 
+let singolaCellaElement = document.querySelectorAll('.cella')
 
 
+
+grigliaElement.addEventListener('click', function (event){
+
+    console.log(event.target.innerHTML)
+
+})
